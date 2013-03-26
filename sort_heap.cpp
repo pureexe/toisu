ad7f93928e4cced9f1a5b1c77709bcd90f6ee011
pakkapon phongtawee <pure1997@windowslive.com>
2013-03-26 11:54:32 +0700

    Update and rename heap_sort; to sort_heap.cpp

--- sort_heap.cpp
+++ sort_heap.cpp
@@ -8,11 +8,13 @@ void print_r(int *arr,int n)
         printf("%d ",arr[i]);
     printf("]\n");
 }
-void addHeap(int in){
+void addHeap(int in)
+{
     int c=rear,tmp;
     heap[rear]=in;
     rear++;
-    while(c>0){
+    while(c>0)
+    {
         if(heap[(c-1)/2]>heap[c])
             break;
         tmp=heap[(c-1)/2];
@@ -46,22 +48,51 @@ int popHeap()
     buildHeap();
     return ans;
 }
-int sortHeap(){
+int sortHeap()
+{
     int tmp;
     while(rear>0)
     {
-    tmp=heap[0];
-    heap[0]=heap[rear-1];
-    heap[rear-1]=tmp;
-    rear--;
-    buildHeap();
+        tmp=heap[0];
+        heap[0]=heap[rear-1];
+        heap[rear-1]=tmp;
+        rear--;
+        buildHeap();
+    }
+}
+void sortHeap2()
+{
+    int i,pos,tmp,c;
+    for(i=rear-1; i>=0; i--)
+    {
+        tmp=heap[0];
+        heap[0]=heap[rear-1];
+        heap[rear-1]=tmp;
+        rear--;
+        c=0;
+        while(2*c+1<rear)
+        {
+            pos=2*c+1;
+            if(pos+1<rear)
+            {
+                if(heap[pos]<heap[pos+1])pos++;
+            }
+            if(heap[pos]>heap[c])
+            {
+                tmp=heap[pos];
+                heap[pos]=heap[c];
+                heap[c]=tmp;
+                c=pos;
+            }
+            else break;
+        }
     }
 }
 int main()
 {
 
     buildHeap();
-    sortHeap();
+    sortHeap2();
     print_r(heap,8);
 
     return 0;
