4fa1deea62908d6afe10a05e6e3371248647c9c2
pakkapon phongtawee <pure1997@windowslive.com>
2013-10-16 19:38:46 +0700

    night 16
    commit finished basic loop day

--- ss1_2013/day2_loopbasic/grader/q09_any_max_consec.cpp
+++ ss1_2013/day2_loopbasic/grader/q09_any_max_consec.cpp
@@ -1,27 +1,44 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-using namespace std;
-struct box{
-    int first;
-    int ct;
-    int cons;
-};
-int cmp(int a,int b){
-
-}
+#include<stdio.h>
 int main()
 {
-    int old=-475418,u;
-    vex
-    while(1){
-            scanf("%d",&u);
-        if(u==0)
-            break;
-        if(old!=u)
+    int old=-4654548,u,consec=-4654548,curcon=0,near,ft=0,fbs;
+    while(1)
+    {
+        scanf("%d",&u);
+        if(ft==0)
         {
-
+            fbs=u;
+            ft=1;
         }
+        if(u==0)
+           {
+               if(consec<curcon)
+                {
+                     consec=curcon;
+                        near=old;
+                }
+
+               break;
+           }
+
+        if(old==u)
+        {
+                curcon++;
+        }
+        else if(old!=u)
+        {
+            if(consec<curcon)
+                {
+                    consec=curcon;
+                    near=old;
+                }
+            curcon=0;
+        }
+        old=u;
+
     }
-    return 0;
+    if(consec!=0)
+    printf("%d\n%d",consec+1,near);
+    else
+    printf("%d\n%d",1,fbs);
 }
