eb9d93289a8cdbcb4b2d848df7f953754b8b27c3
pakkapon phongtawee <pure1997@windowslive.com>
2013-10-22 16:25:00 +0700

    update permutation

---
+++ ss1_2013/day8_quest/teach/permu_recursive.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int n=6,K=3;
+int A[]={1,2,3,4,5,6},U[10];
+
+int run(int bf,int af){
+   int i;
+    if(bf==K)
+    {
+        ct++;
+        for(i=0;i<K;i++)
+            printf("%d ",U[i]);
+        printf("\n");
+        return 0;
+    }
+    for(i=af-1;i>=0;i--)
+    {
+        U[bf]=A[i];
+        run(bf+1,i);
+    }
+}
+
+int main(){
+    run(0,n);
+    printf("\n\n\n%d",ct);
+}
